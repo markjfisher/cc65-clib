b0db4b2791e1ce0c6e34bcd7f4559b603bc3998d
Mark Fisher <mark.j.fisher@gmail.com>
2025-09-13 19:11:35 +0100

    remove bbc string buff

--- src/libsrc/bbc/open.c
+++ src/libsrc/bbc/open.c
@@ -5,49 +5,51 @@
 #include <errno.h>
 #include <stdarg.h>
 
-//??? doesnt work for all flags yet! danger will overwrite when it shouldnt!
+// This implementation requires fixing. Not all flags are supported
 
 unsigned char __fastcall__ osfind(unsigned char mode, const char *name);
 int __fastcall__ close_file(unsigned char channel);
-extern char *bbc_string_buf;
 
 int open(const char *name, int flags, ...) {
     char *p;
     unsigned char channel;
     unsigned char fd_flags;
     int fd;
-        
-    // Copy bbc_string_buf to avoid modifying the original string
-    strcpy(bbc_string_buf, name);
-    
+    char filename[128];
+
+    // Copy to filename to avoid modifying the original string
+    // strncpy copies up to 127 chars and null-terminates, leaving room for $0D
+    strncpy(filename, name, 127);
+    filename[127] = '\0';  // Ensure null termination
+
     // bbc requires $0D as filename terminator
-    p = strchr(bbc_string_buf, '\0');
+    p = strchr(filename, '\0');
     *p = '\x0D';
-    
+
     // Fix the flag checking logic
     if ((flags & O_RDWR) == O_RDONLY) {
-        channel = osfind(0x40, bbc_string_buf);
+        channel = osfind(0x40, filename);
         fd_flags = FD_FLAG_READ;
     } else if ((flags & O_RDWR) == O_WRONLY) {
-        channel = osfind(0x80, bbc_string_buf);
+        channel = osfind(0x80, filename);
         fd_flags = FD_FLAG_WRITE;
     } else if ((flags & O_RDWR) == O_RDWR) {
-        channel = osfind(0xc0, bbc_string_buf);
+        channel = osfind(0xc0, filename);
         fd_flags = FD_FLAG_READ | FD_FLAG_WRITE;
     } else {
         channel = 0; //error;
     }
-        
+
     if (!channel) {
         __errno = EIO;    // Use errno symbol
         return -1;
     }
-    
+
     fd = _fd_getfree(channel, fd_flags);
-    
+
     if (fd == -1) {
         close_file(channel);
     }
-        
+
     return fd;
 }
--- src/libsrc/bbc/oslib/bbc_string_buf.s
+++
@@ -1,9 +0,0 @@
-;
-; Dominic Beesley 2005
-; OSLib implementation for BBC/Master Target        
-;
-; Used by various functions for string manipulations
-        .export _bbc_string_buf
-
-        .bss
-_bbc_string_buf:		.res 255, 0
